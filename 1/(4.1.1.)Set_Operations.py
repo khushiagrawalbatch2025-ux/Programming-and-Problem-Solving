e5fc2b7e6f67efb8541b1558d3cf43f868fd4abd
khushiagrawalbatch2025-ux <khushi.agrawal.batch2025@sitnagpur.siu.edu.in>
2026-02-12 23:14:06 +0530

    Delete 1 directory

--- 1/(4.1.1.)Set_Operations.py
+++
@@ -1,8 +0,0 @@
-set_A = set(map(int, input("Set A: ").split()))
-set_B = set(map(int,input("Set B: ").split()))
-uni_set=set_A | set_B
-print ("Union:",uni_set)
-inter_set= set_A & set_B
-print("Intersection:", inter_set)
-diff_set=set_A - set_B
-print("Difference:", diff_set)
