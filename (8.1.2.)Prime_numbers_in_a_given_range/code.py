43da4c3cd7be5ed66f8533edd1f7c40472cb3956
khushiagrawalbatch2025-ux <khushi.agrawal.batch2025@sitnagpur.siu.edu.in>
2026-03-04 10:10:43 +0530

    A prime number finder in a given range

---
+++ (8.1.2.)Prime_numbers_in_a_given_range/code.py
@@ -0,0 +1,15 @@
+start = int(input())
+end = int(input())
+
+found = False
+for num in range(start, end + 1):
+    if num > 1:
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                break
+        else:
+            print(num)
+            found = True
+
+if not found:
+    print("No primes")
