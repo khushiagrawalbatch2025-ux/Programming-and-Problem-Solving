5a68318d96527257bd398a65ac46381d6da96e55
khushiagrawalbatch2025-ux <khushi.agrawal.batch2025@sitnagpur.siu.edu.in>
2026-03-04 09:43:07 +0530

    matrix multiplication for two square matrices

--- (8.1.1.)Multiplication_of_two_square_matrix/code.py
+++ (8.1.1.)Multiplication_of_two_square_matrix/code.py
@@ -1,1 +1,19 @@
+n = int(input("dimension: "))
 
+print("first matrix:")
+matrix1 = [list(map(int, input().split())) for _ in range(n)]
+
+print("second matrix:")
+matrix2 = [list(map(int, input().split())) for _ in range(n)]
+
+result = [[0 for _ in range(n)] for _ in range(n)]
+
+
+for i in range(n):
+  for j in range(n):
+    for k in range(n):
+      result[i][j] += matrix1[i][k] * matrix2[k][j]
+
+print("Resultant Matrix:")
+for row in result:
+  print(*(row))
